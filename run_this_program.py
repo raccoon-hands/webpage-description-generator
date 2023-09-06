7e5248b37cfe37cf2ffc3b528b4c6935806c1c6c
raccoon-hands <105438087+raccoon-hands@users.noreply.github.com>
2023-09-06 13:38:19 +0100

    Now writes to file after each iteration
    -added function save_description()
    -changed the layout of the code to be more readable
    -script now writes to file after every iteration and no longer saves once the entire process is complete

--- run_this_program.py
+++ run_this_program.py
@@ -12,24 +12,8 @@ import math
 websites = []
 loop = True
 
-#Collect the csv file name to read/ write to via user input.
-while (loop == True):
-    print("Please input the name of your CSV file:")
-    filename = input("")
-    try:
-        my_csv = pd.read_csv(filename)
-        loop = False
-    except:
-        print("Error: Failed to read file. Please note:")
-        print("-The file should be a .csv file in the same directory as this program.")
-        print("-Your input should not be enclosed in quotation marks.")
-        print("-You should include the .csv extension at the end of your file name.")
-        print()
-
-"""The following code is what allows us to pick up where we left
-off on the same csv file"""
+"""remove_nan_from_end removes nan values from the end of a list"""
 def remove_nan_from_end(lst):
-#removes nan values from the end of a list
     reversed_lst = list(reversed(lst))
     result = []
     found_non_nan = False
@@ -44,6 +28,54 @@ def remove_nan_from_end(lst):
         result.append(item)
 
     return list(reversed(result))
+
+"""recover_descriptions is used to recover the descriptions and bad urls generated this session"""
+def recover_descriptions():
+    padding_length = len(my_csv) - len(long_desc_col)
+    padded_values = long_desc_col + [''] * padding_length
+    my_csv['Long Description'] = padded_values
+
+    padding_length = len(my_csv) - len(short_desc_col)
+    padded_values = short_desc_col + [''] * padding_length
+    my_csv['Short Description'] = padded_values
+
+    padding_length = len(my_csv) - len(shortest_desc_col)
+    padded_values = shortest_desc_col + [''] * padding_length
+    my_csv['Rollover Description'] = padded_values
+
+    my_csv.to_csv(filename, index=False)
+    all_denied_urls_csv = pd.DataFrame({'Denied URLs': all_denied_urls})
+    all_denied_urls_csv.to_csv("denied_urls.csv", index=False)
+    print("Descriptions recovered successfully.")
+
+"""save_descriptions writes the generated descriptions to the csv file"""
+def save_descriptions():
+    padding_length = len(my_csv) - len(long_desc_col)
+    padded_values = long_desc_col + [''] * padding_length
+    my_csv['Long Description'] = padded_values
+
+    padding_length = len(my_csv) - len(short_desc_col)
+    padded_values = short_desc_col + [''] * padding_length
+    my_csv['Short Description'] = padded_values
+
+    padding_length = len(my_csv) - len(shortest_desc_col)
+    padded_values = shortest_desc_col + [''] * padding_length
+    my_csv['Rollover Description'] = padded_values
+    my_csv.to_csv(filename, index=False)
+
+#Collect the csv file name to read/ write to via user input.
+while (loop == True):
+    print("Please input the name of your CSV file:")
+    filename = input("")
+    try:
+        my_csv = pd.read_csv(filename)
+        loop = False
+    except:
+        print("Error: Failed to read file. Please note:")
+        print("-The file should be a .csv file in the same directory as this program.")
+        print("-Your input should not be enclosed in quotation marks.")
+        print("-You should include the .csv extension at the end of your file name.")
+        print()
 
 #check for existing descriptions
 last_valid_index = my_csv['Rollover Description'].last_valid_index()
@@ -67,7 +99,6 @@ else: #if there are existing descriptions already
 #if this is a new file it'll start at the beginning
 websites = my_csv['Tool URL'][start_index:].tolist()
 
-
 """The following code handles the description generation and the writing
 of the csv files"""
 #Initialise variables for time elapsed
@@ -78,32 +109,10 @@ overall_start = time.perf_counter()
 #initialise list of denied URLS
 denied_urls_csv = pd.read_csv("denied_urls.csv")
 all_denied_urls = denied_urls_csv["Denied URLs"].tolist()
-denied_urls_list = []
 
 #If GPT response contains these, it means it can't generate a description.
 bad_phrases = ["I'm sorry", "I apologize", "JavaScript"] 
-
-#This function will recover the descriptions and bad urls generated this session:
-def recover_descriptions():
-    padding_length = len(my_csv) - len(long_desc_col)
-    padded_values = long_desc_col + [''] * padding_length
-    my_csv['Long Description'] = padded_values
-
-    padding_length = len(my_csv) - len(short_desc_col)
-    padded_values = short_desc_col + [''] * padding_length
-    my_csv['Short Description'] = padded_values
-
-    padding_length = len(my_csv) - len(shortest_desc_col)
-    padded_values = shortest_desc_col + [''] * padding_length
-    my_csv['Rollover Description'] = padded_values
-
-    my_csv.to_csv(filename, index=False)
-    for url in denied_urls_list:
-        all_denied_urls.append(url)
-    all_denied_urls_csv = pd.DataFrame({'Denied URLs': all_denied_urls})
-    all_denied_urls_csv.to_csv("denied_urls.csv", index=False)
-    print("Descriptions recovered successfully.")
-
+    
 #Iterate through the list of websites and generate 3 descriptions per URL 
 for website in websites:
     print("Accessing", website, "...")
@@ -116,25 +125,31 @@ for website in websites:
         
     except:
         iterator += 1
-        denied_urls_list.append(website)#add URL to list of denied urls
+        all_denied_urls.append(website)#add URL to list of denied urls
+        all_denied_urls_csv = pd.DataFrame({'Denied URLs': all_denied_urls})
+        all_denied_urls_csv.to_csv("denied_urls.csv", index=False)
         long_desc_col.append("") #add blanks to the description lists
         short_desc_col.append("")
         shortest_desc_col.append("")
+        save_descriptions()
         print("Unable to generate description. Website added to denied_urls.csv.")
         print(">>>>>>>>>", iterator, " out of ", length, " websites complete.")
         print()
         continue
     
-    page_text = truncate_text(page_text) #shortens to 600 words to avoid token limit
+    #shorten to 600 words to avoid token limit
+    page_text = truncate_text(page_text) 
 
     #Ask GPT for descriptions of the resource
     try:
         long_desc = gpt.long_describe(page_text)
         short_desc = gpt.short_describe(page_text)
         shortest_desc = gpt.link_describe(page_text)
+        
+    #If the rate limit has been exceeded, stop executing.
     except Exception as e:
-        print(f"Error: {str(e)}") #If the rate limit has been exceeded, recover
-        print()                   #descriptions and stop executing.
+        print(f"Error: {str(e)}") 
+        print()                   
         recover_descriptions()    
         print("Process exited. You may now close the program.")
         sys.exit()
@@ -142,7 +157,7 @@ for website in websites:
     iterator += 1
     end = time.perf_counter() #end the timer for this iteration
     elapsed = round(end - start)
-     if elapsed < 62:
+    if elapsed < 62:
         naptime = 62 - elapsed #calculate time to back off for
     else:
         naptime = False
@@ -158,11 +173,16 @@ for website in websites:
             found_phrases = True
             
     if found_phrases:
-        denied_urls_list.append(website)#add URL to list of denied urls
+        #add URL to list of denied urls
+        all_denied_urls.append(website)
+        all_denied_urls_csv = pd.DataFrame({'Denied URLs': all_denied_urls})
+        all_denied_urls_csv.to_csv("denied_urls.csv", index=False)
+        
         long_desc_col.append("") #add blanks to the description lists
         short_desc_col.append("")
         shortest_desc_col.append("")
-        print("Unable to generate description. Website added to denied_urls.csv.")
+        save_descriptions()
+        print("Unable to generate description. URL added to denied_urls.csv.")
     else:
         long_desc_col.append(long_desc) #else add descriptions to lists
         short_desc_col.append(short_desc)
@@ -174,6 +194,8 @@ for website in websites:
         print("********ROLLOVER DESCRIPTION********")
         print(shortest_desc)
         print()
+        save_descriptions()
+        print("Descriptions saved to", filename)
         
     print(">>>>>>>>>", iterator, " out of", length, "websites complete.")
     print("Time elapsed:", elapsed, "seconds.")
@@ -183,7 +205,6 @@ for website in websites:
         time.sleep(naptime)
         print()
         
-    
         
 overall_end = time.perf_counter() #stop the timer
 overall_elapsed = overall_end - overall_start   #calculate overall time elapsed
@@ -191,23 +212,4 @@ print()
 print("Process complete. Total time elapsed:", overall_elapsed, "seconds.")
 #Hooray!
 
-print()
-print("Writing descriptions to .csv file...")
-#writes the descriptions to the csv file
-my_csv["Long Description"] = long_desc_col
-my_csv["Short Description"] = short_desc_col
-my_csv["Rollover Description"] = shortest_desc_col
-
-my_csv.to_csv(filename, index=False)
-
-print("Writing unscraped urls to .csv file...")
-#appends the new list of unscrapable urls to the existing list
-#and writes to denied_urls.csv
-for url in denied_urls_list:
-    all_denied_urls.append(url)
-
-all_denied_urls_csv = pd.DataFrame({'Denied URLs': all_denied_urls})
-
-all_denied_urls_csv.to_csv("denied_urls.csv", index=False)
-
-print("Writing complete. You may now close the program.")
+print("You may now close the program.")
